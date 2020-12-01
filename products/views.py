6c3d9a2fd97cef09ffed4f22aab47522c215d609
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-12-01 02:28:27 +0100

    :wrench: Added context for products & detail

--- products/views.py
+++ products/views.py
@@ -1,6 +1,17 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 
-def products(request, *args, **kwargs):
-    return render(request, "products.html", {})
+def product_all(request, *args, **kwargs):
+    context = {
+        "list": Product.objects.all()
+    }
+    return render(request, "product/products.html", context)
+
+
+def product_detail(request, *args, **kwargs):
+    obj = Product.objects.get(id=request.GET['id'])
+    context = {
+        "object": obj
+    }
+    return render(request, "product/detail.html", context)
